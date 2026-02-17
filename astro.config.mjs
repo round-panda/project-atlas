bd785abf7bc3996d58353d5ba6353aa85955a515
round-panda <132237902+round-panda@users.noreply.github.com>
2026-02-17 01:12:38 -0800

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Set site when you know the final URL, e.g.:
   // site: 'https://projectatlas.tcusd.org',
+  site: 'https://round-panda.github.io',
+  base: '/project-atlas',
 });
